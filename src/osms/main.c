c2c7a2b031568c0ecfa060e0b974feba258a7754
benjasaldias <benjamn.saldas@uc.cl>
2025-05-22 20:44:17 -0400

    cambios

--- src/osms/main.c
+++ src/osms/main.c
@@ -7,5 +7,7 @@ int main(int argc, char const *argv[])
     // montar la memoria
     os_mount((char *)argv[1]);
     os_start_process(3, "ventas");
+    os_ls_processes();
+    fclose(memory_file);
     return 0;
 }
